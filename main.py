6fe4955d0fb200e5e3bf0755c98ded016af102df
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-11-29 23:20:35 +0100

    Add configuration ti main.py.

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ from views import home
 
 api = fastapi.FastAPI()
 
-# Opt in making fastAPI pick the 'static' dir to use it
-api.mount('/static', StaticFiles(directory='static'), name='static')
-# Setup routers
-api.include_router(home.router)
-api.include_router(weather_api.router)
+
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    # Opt in making fastAPI pick the 'static' dir to use it
+    api.mount('/static', StaticFiles(directory='static'), name='static')
+    # Setup routers
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
 
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8005, host='127.0.0.1')
+else:
+    configure()
