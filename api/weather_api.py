378e023bf3c32137eb959e010337ab93b76a2d2b
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-08 22:13:46 +0100

    Utilize Location model. Add more informative comments.

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 import fastapi
+from fastapi import Depends
+
+from models.Location import Location
 
 router = fastapi.APIRouter()
 
 
+# By passing additional requirements to the function, we indicate that they are queries.
+# By using "Optional" we communicate that they are optional values and can have a default if not passed.
+# The use of Depends() allows the pydantic model to search everywhere in the request and not just in the body. This allowds the detection of variables that are in the request url
 @router.get('/api/weather/{city}')
-def weather():
-    return "some report"
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    return f"{loc.city}, {loc.state}, {loc.country} in {units}"
