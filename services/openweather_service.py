fa9756ca8b718c0872b4b0d0e9e61f4d1d8c6d0b
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-08 22:52:21 +0100

    Enhance openweather_service

--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,8 +1,10 @@
 from typing import Optional
 
+# This key will be set by the configure_apikeys function on startup
+api_key: Optional[str] = None
+
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
     query = f'{city},{country}'
-    key = 132
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={query}&appid={key}?units={units}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}?units={units}'
     print(url)
