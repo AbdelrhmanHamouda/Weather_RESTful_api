d007a00ef41839f0b2d48f9dc3246a7046d255eb
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-08 22:52:00 +0100

    Implement configure_apikeys()

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+import json
+from pathlib import Path
+
 import fastapi
 import uvicorn
 from starlette.staticfiles import StaticFiles
 
 from api import weather_api
+from services import openweather_service
 from views import home
 
 api = fastapi.FastAPI()
 
 
 def configure():
+    """
+    This function is used to configure the service
+    """
     configure_routing()
+    configure_apikeys()
+
+
+def configure_apikeys():
+    """
+    Use this function to get the API key
+    """
+    file = Path('settings.json').absolute()
+    if not file.exists():
+        print(f'WARNING: {file} file not found, you cannot continue, please see settings_template.json')
+        raise Exception("settings.json file not found, you cannot continue, please see settings_template.json")
+    with open('settings.json') as file_handler:
+        settings = json.load(file_handler)
+        openweather_service.api_key = settings.get('api_key')
 
 
 def configure_routing():
