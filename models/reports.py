a763ccf4468da8013f64cb5ff74c6d51460c9ffb
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-13 21:03:14 +0100

    Add ReportSubmit model and get Report to subclass it

--- models/reports.py
+++ models/reports.py
@@ -6,7 +6,11 @@ from pydantic import BaseModel
 from models.Location import Location
 
 
-class Report(BaseModel):
+class ReportSubmit(BaseModel):
     description: str
     location: Location
+
+
+class Report(ReportSubmit):
+    id: str
     created_date: Optional[datetime.datetime]
