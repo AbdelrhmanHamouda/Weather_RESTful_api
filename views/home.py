9bfa5c5bf53053b5ce1cb94794ec9dfa822dfd71
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-13 21:51:40 +0100

    Exclude favicon.ico and root from documentation

--- views/home.py
+++ views/home.py
@@ -9,7 +9,7 @@ templates = Jinja2Templates('templates')
 router = fastapi.APIRouter()
 
 
-@router.get('/')
+@router.get('/', include_in_schema=False)
 async def index(request: Request):
     events = await report_service.get_reports()
     data = {'request': request, 'events': events}
@@ -18,6 +18,6 @@ async def index(request: Request):
     return templates.TemplateResponse('home/index.html', data)
 
 
-@router.get('/favicon.ico')
+@router.get('/favicon.ico', include_in_schema=False)
 def favicon():
     return fastapi.responses.RedirectResponse(url='/static/img/favicon.ico')
