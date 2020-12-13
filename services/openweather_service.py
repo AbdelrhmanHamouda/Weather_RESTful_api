7e6f8f922f7128733e27cd570da745c1866cb4fc
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-13 11:49:18 +0100

    Implement getting the weather data from openweathermap.org

--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,10 +1,29 @@
 from typing import Optional
 
 # This key will be set by the configure_apikeys function on startup
+import requests
+
 api_key: Optional[str] = None
 
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    query = f'{city},{country}'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}?units={units}'
-    print(url)
+    # Change the query based on the provided params
+    if state:
+        query = f'{city},{state},{country}'
+    else:
+        query = f'{city},{country}'
+
+    # Construct the request url
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={query}&units={units}&appid={api_key}'
+
+    # Send request
+    response = requests.get(url)
+
+    # Parse data from json to dict
+    data = response.json()
+
+    # extract the 'main' section
+    forecast = data['main']
+
+    # Return forecast
+    return forecast
