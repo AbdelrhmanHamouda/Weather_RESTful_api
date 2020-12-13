9b476909d2d1d5cc32cd3b8ef4ac16d5a4d7d565
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-13 20:30:18 +0100

    Init commit

---
+++ services/report_service.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from models.Location import Location
+
+# This is JUST to simplify the implementation, normally, this needs to be done through a database.
+__report = []
+
+
+def get_reports() -> List:
+    return []
+
+
+def add_report(description: str, location: Location):
+    pass
